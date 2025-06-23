3bd62fb7f9073c6020579c25596dbfc76511d742
VanshBhatia2192 <vanshbhatia767@gmail.com>
2025-06-23 13:50:12 +0530

    added window navigation shortcuts

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,3 +19,19 @@ keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
 keymap.set("n", "<leader>dd", vim.diagnostic.open_float)
+
+-- Move between windows using Ctrl + h/j/k/l
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Move focus from file to nvim-tree and back
+keymap.set("n", "<C-b>", function()
+  local view = require('nvim-tree.view')
+  if view.is_visible() then
+    view.focus()
+  else
+    vim.cmd('NvimTreeToggle')
+  end
+end, { desc = "Focus or toggle NvimTree" })
