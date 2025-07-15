2c2c3d29ea63ebb4fe7943953a1205e09a223044
VanshBhatia2192 <vanshbhatia767@gmail.com>
2025-07-15 14:29:23 +0530

    fixed some warnings

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -34,7 +34,7 @@ return {
 			local on_attach = function(client, bufnr)
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
 				vim.keymap.set("n", "od", function()
-					local params = vim.lsp.util.make_position_params()
+					local params = vim.lsp.util.make_position_params(0, "utf-8")
 					vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result)
 						if not result or vim.tbl_isempty(result) then
 							vim.notify("No definition found", vim.log.levels.INFO)
@@ -42,7 +42,7 @@ return {
 						end
 						local def = (vim.islist(result) and result[1]) or result
 						vim.cmd("tabnew")
-						vim.lsp.util.jump_to_location(def, "utf-8")
+						vim.lsp.util.show_document(def, "utf-8")
 					end)
 				end, { noremap = true, silent = true, desc = "Open definition in new tab" })
 				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
