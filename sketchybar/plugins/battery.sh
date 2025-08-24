928fca3ef109274bca9a9bd1ad7693dbea40410e
VanshBhatia2192 <vanshbhatia767@gmail.com>
2025-08-24 12:59:31 +0530

    added percentage display

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-
 source "$HOME/.config/sketchybar/icons.sh"
 source "$HOME/.config/sketchybar/colors.sh"
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
-if [ $PERCENTAGE = "" ]; then
+if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
 DRAWING=on
 COLOR=$WHITE
+
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
+  9[0-9]|100) ICON=$BATTERY_100;
   ;;
-  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
+  [6-8][0-9]) ICON=$BATTERY_75;
   ;;
   [3-5][0-9]) ICON=$BATTERY_50
   ;;
@@ -26,7 +26,14 @@ esac
 
 if [[ $CHARGING != "" ]]; then
   ICON=$BATTERY_CHARGING
-  DRAWING=off
+  DRAWING=on
 fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+# Set both icon and label (percentage text)
+sketchybar --set $NAME \
+  drawing=$DRAWING \
+  icon="$ICON" \
+  icon.color=$COLOR \
+  label="${PERCENTAGE}%" \
+  label.color=$COLOR \
+  label.drawing=on
